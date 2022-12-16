dfb89448218a259937529090722fb1b024f1cf12
Marelyn Deschamps <mdeschampsfranco@gmail.com>
2022-12-16 16:09:22 -0400

    first commit
    16-12-22

--- src/pages/Detalles.jsx
+++ src/pages/Detalles.jsx
@@ -1,5 +1,10 @@
+import {Link, useParams} from "react-router-dom";
+import { useFetch } from "../utils/hooks/useFetch";
 
 function Detalles(){
+    const params = useParams();
+    const {data, isLoading} = useFetch(`http://localhost:3000/users/${params.id}`);
+    if(isLoading) return <h1></h1>
     return(<div>
         <h1>Hola</h1>
     </div>)
